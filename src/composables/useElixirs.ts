07984b5f70a8ce8635dfc6103235373c6fe746d7
Porralicious <helder.jose.fernandes@gmail.com>
2025-06-16 21:59:58 +0200

    Add elixir detail view

--- src/composables/useElixirs.ts
+++ src/composables/useElixirs.ts
@@ -2,8 +2,15 @@ import { useQuery } from '@tanstack/vue-query'
 import axios from 'axios'
 import type { Elixir } from '@/types/Elixir'
 
+const API_BASE = 'https://wizard-world-api.herokuapp.com'
+
 const fetchElixirs = async (): Promise<Elixir[]> => {
-  const { data } = await axios.get('https://wizard-world-api.herokuapp.com/Elixirs')
+  const { data } = await axios.get(`${API_BASE}/Elixirs`)
+  return data
+}
+
+const fetchElixirById = async (id: string): Promise<Elixir> => {
+  const { data } = await axios.get(`${API_BASE}/Elixirs/${id}`)
   return data
 }
 
@@ -12,5 +19,14 @@ export function useElixirs() {
     queryKey: ['elixirs'],
     queryFn: fetchElixirs,
     staleTime: 1000 * 60 * 5,
+  })
+}
+
+export function useElixir(id: string) {
+  return useQuery({
+    queryKey: ['elixir', id],
+    queryFn: () => fetchElixirById(id),
+    enabled: !!id, // only fetch if id is defined
+    staleTime: 1000 * 60 * 10,
   })
 }
